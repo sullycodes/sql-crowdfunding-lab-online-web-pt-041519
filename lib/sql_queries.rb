ab88014ee5a8788f6cc249a9b40fb0a7fc274f0f
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-24 15:11:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,9 +42,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT 
-  FROM 
-  INNER JOIN 
+  "SELECT projects.category, pledges.amount
+  FROM projects
+  INNER JOIN pledges
   ON 
   GROUP BY" 
 end
