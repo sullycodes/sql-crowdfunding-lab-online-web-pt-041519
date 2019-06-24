70ff2717360703b1f41113f3d6d4b7664a7b49da
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-24 13:59:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  SELECT name
+  SELECT employee, SUM(bonus) 
+  FROM employee_bonus 
+  GROUP BY employee 
+  HAVING SUM(bonus) > 1000;
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
