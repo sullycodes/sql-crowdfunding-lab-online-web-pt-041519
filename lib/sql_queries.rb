5df6bec8331eab7462ce924c2aa381185d43ee0f
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-24 15:30:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,11 +44,8 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
   FROM projects
-  INNER JOIN pledges
-  WHERE projects.category = 'music'"
-  # ON projects.category = \"music\" "
-  # WHERE projects.category = \"music\" 
-  # ORDER BY pledges.amount" 
+  INNER JOIN pledges"
+  # WHERE projects.category = 'music'
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
