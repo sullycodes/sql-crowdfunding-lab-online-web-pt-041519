ecd5070d3cbccfc9204ca26448fe1bae3f80b535
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-24 14:33:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   # GROUP BY title
   # HAVING SUM(pledges.amount)"
   # "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id"
-  "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges WHERE project_id = 1 GROUP BY title"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges WHERE project_id = pledges.project_id GROUP BY title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
