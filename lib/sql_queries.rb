3a97abe58ef70fa0450e3b12d2454a66a119a27b
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-24 15:02:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, pledges.amount
   FROM users
   INNER JOIN pledges
-  ON users.id, pledges.user_id
+  ON users.id = pledges.user_id
   GROUP BY users.name 
   ORDER BY pledges.amount
   "
